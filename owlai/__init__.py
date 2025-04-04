f649cc1628d73ff89bb20a9c74f20045c847fe5e
Fred Brunner <frdbrunner@gmail.com>
2025-04-04 15:04:22 +0200

    Improved code structure and added a new fly.py file for interactive mode

--- owlai/__init__.py
+++ owlai/__init__.py
@@ -5,9 +5,9 @@ OwlAI - A framework for building sentient AI agents
 from owlai.core import OwlAgent
 from owlai.rag import RAGAgent
 from owlai.tools import ToolBox
-from owlai.agent_manager import AgentManager
+from owlai.nest import AgentManager
 
-__version__ = "0.1.0"
+__version__ = "0.3.0"
 
 __all__ = [
     "OwlAgent",
